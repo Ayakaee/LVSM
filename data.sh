3857dc45f17fc2db6b34ff57a9e0d430a38d0a04
dhmbb2 <yjqsjtu2022@sjtu.edu.cn>
2025-07-08 15:58:43 +0800

    1

--- data.sh
+++
@@ -1,3 +0,0 @@
-# rm /inspire/ssd/ws-f4d69b29-e0a5-44e6-bd92-acf4de9990f0/public-project/public/yjc/gwq/dataset/processed_re10k -r
-python process_data.py --base_path /inspire/ssd/ws-f4d69b29-e0a5-44e6-bd92-acf4de9990f0/public-project/public/yjc/gwq/dataset/re10k --output_dir /inspire/ssd/ws-f4d69b29-e0a5-44e6-bd92-acf4de9990f0/public-project/public/yjc/gwq/dataset/processed_re10k --mode 'train'
-# bash train.sh
